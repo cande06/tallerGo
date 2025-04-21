a464d0363fb97cd59a21480675b269e605e83cc3
Cande <candelaxnahir@gmail.com>
2025-04-21 16:48:26 -0300

    enhacement

--- ex2/main.go
+++ ex2/main.go
@@ -4,14 +4,52 @@ import (
 	"errors"
 	"fmt"
 
+	"math/rand"
+
 	"github.com/fatih/color"
 )
 
 func main() {
-	color.Green("hyola")
+	// color.Green("hyola")
 
-	var test Dispositivo
-	fmt.Println(test.EstadoActual())
+	var devices = []Dispositivo{
+		{"Lampara", true},
+		{"TV", false},
+		{"Estereo", false},
+		{"Router", true},
+		{"AC", true},
+		{"Cerradura", true},
+		{"Garage", false},
+	}
+
+	for _, device := range devices {
+		fmt.Println("\n", device.Nombre)
+
+		v := rand.Intn(10)
+		if (v % 2) == 0 {
+			fmt.Println("Encendiendo...")
+
+			on := device.Encender()
+			if on != nil {
+				fmt.Println("error->", on)
+			}
+
+			fmt.Print("Estado: ")
+			color.Green(device.EstadoActual())
+
+		} else {
+			fmt.Println("Apagando...")
+
+			off := device.Apagar()
+			if off != nil {
+				fmt.Println("error->", off)
+			}
+
+			fmt.Print("Estado: ")
+			color.Red(device.EstadoActual())
+
+		}
+	}
 }
 
 // ## ESTRUCTURA
@@ -28,26 +66,29 @@ type Controlable interface {
 }
 
 // ## FUNCIONES
-func (d *Dispositivo) Encender() error {
-	if d.Estado {
-		return errors.New("el dispositivo ya está encendido")
-	}
-	d.Estado = false
-	return errors.New("")
-}
 
-func (d *Dispositivo) Apagar() error {
-	if !d.Estado {
-		return errors.New("el dispositivo ya está apagado")
-	}
-	d.Estado = false
-	return errors.New("")
-}
-
+// Devolver estado del dispositivo
 func (d Dispositivo) EstadoActual() string {
 	if !d.Estado {
 		return "Apagado"
 	} else {
 		return "Encendido"
 	}
+}
+
+// Definir errores
+func (d *Dispositivo) Encender() error {
+	if d.Estado {
+		return errors.New("el dispositivo ya se encuentra encendido")
+	}
+	d.Estado = true
+	return nil
+}
+
+func (d *Dispositivo) Apagar() error {
+	if !d.Estado {
+		return errors.New("el dispositivo ya se encuentra apagado")
+	}
+	d.Estado = false
+	return nil
 }
